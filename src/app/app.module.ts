b111f891e5bf4c70ab5dc5675130bd514c18bacd
Gopika Kabra <gopika.kabra@gmail.com>
2017-11-08 19:15:16 +0530

    Added Routing and navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { UsersComponent } from './users/users.component';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './users/user/user.component';
+import { EditUserComponent } from './users/edit-user/edit-user.component';
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent },
+  {path: 'users', component: UsersComponent },
+  {path: 'users/:id/:name', component: UserComponent }
+];
 
 // @ngmodule and likes are a called decorator in angular, they start with @ sign
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    UsersComponent,
+    HomeComponent,
+    UserComponent,
+    EditUserComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
